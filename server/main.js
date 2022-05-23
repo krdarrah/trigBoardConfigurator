92f077da1274ff5ec36f0990b20260f5ee5b71de
mdconaway <mdconaway@users.noreply.github.com>
2022-05-22 22:39:22 -0400

    -Add local node https server

---
+++ server/main.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const express = require('express');
+const https = require('https');
+const CertificateFactory = require('./CertificateFactory.js');
+const app = express();
+const httpsOptions = {};
+
+app.use(express.static(path.resolve(__dirname, '../'), {'index': ['index.html']}));
+const selfSignedCertificateFactory = new CertificateFactory({});
+Object.assign(httpsOptions, selfSignedCertificateFactory.getStaticBundle('buffer'));
+https.createServer(httpsOptions, app).listen(1337);
+
+console.log('Trigboard server running at https://localhost:1337/')
